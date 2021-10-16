7df5c7c199878484e36742287e513d5a249b466b
Gaurav Tewari <gauravtewari111@gmail.com>
2021-10-16 20:07:25 +0530

    feat: card rending added

--- src/App.js
+++ src/App.js
@@ -13,21 +13,43 @@ export const AnimeList = gql`
         native
       }
       description
+      coverImage {
+        medium
+      }
+      bannerImage
+      volumes
+      episodes
     }
+  
   }
 }
+
 `;
 
 function App() {
   const {loading, error, data} = useQuery(AnimeList);
 
-  console.log(data); 
+  console.log(data?.Page?.media[0]); 
   if(loading) return(<> Loading</>);
   if(error) return(<>{JSON.stringify(error)}</>)
   return (
-   <>
-   {JSON.stringify(data)}
-   </>);
+   <div className="container"> 
+     <h1> 🐈 Anime List </h1>
+     <hr width="80%" />
+   {data?.Page?.media.map(anime => (
+     <>
+   <div className="card" >
+      <img    src={anime.coverImage.medium}/>
+      <div> 
+         <h1>{anime.title.english} </h1>
+           <div className="episodes" >Episodes  <b>{anime.episodes} </b></div>
+          <div  dangerouslySetInnerHTML={{__html: anime.description}} ></div> 
+      </div> 
+  </div>
+  <hr width="75%"/>
+ </>
+   ))}
+   </div>);
 }
 
 export default App;
